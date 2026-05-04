34dc8096bc529433551f5da2ec28160b10be6411
vk-traders-318 <vktraders.office.1@gmail.com>
2026-05-04 10:28:06 +0530

    Delete server directory

--- server/models/OTP.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const OTPSchema = new mongoose.Schema({
-    email: String,
-    otp: String,
-    expiresAt: Date
-});
-
-module.exports = mongoose.model("OTP", OTPSchema);
