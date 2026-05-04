701b64f16cf691e34ff4331e87b587198fc27d88
vk-traders-318 <vktraders.office.1@gmail.com>
2026-05-04 10:08:25 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,1 +1,24 @@
+require("dotenv").config();
 
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+require("./db");
+
+app.use(cors());
+app.use(express.json());
+
+const authRoutes = require("./routes/auth");
+const chatRoutes = require("./routes/chat");
+
+app.use("/api/auth", authRoutes);
+app.use("/api/chat", chatRoutes);
+
+app.get("/", function (req, res) {
+    res.send("Server Running");
+});
+
+app.listen(process.env.PORT, function () {
+    console.log("Server started on port " + process.env.PORT);
+});
