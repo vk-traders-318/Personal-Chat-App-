c77f47be9fed8efb293b451cf25d1829f186bdd9
vk-traders-318 <vktraders.office.1@gmail.com>
2026-05-04 17:08:26 +0530

    Update mail.js

--- utils/mail.js
+++ utils/mail.js
@@ -3,22 +3,12 @@ const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendOTP(email, otp) {
-    try {
-        console.log("Sending OTP to:", email);
-
-        const response = await resend.emails.send({
-            from: "Chat App <noreply@personalchatapp.in>",
-            to: email,
-            subject: "Your OTP Code",
-            html: `<h2>Your OTP is: ${otp}</h2>`
-        });
-
-        console.log("OTP sent successfully:", response);
-
-    } catch (err) {
-        console.log("MAIL ERROR:", err.message);
-        throw err;
-    }
+    await resend.emails.send({
+        from: "Chat App <noreply@personalchatapp.in>",
+        to: email,
+        subject: "Your OTP",
+        html: `<h2>Your OTP is ${otp}</h2>`
+    });
 }
 
 module.exports = { sendOTP };
