a156729e2bfb48b0855d3468fe7e3369b2a9c486
vk-traders-318 <vktraders.office.1@gmail.com>
2026-05-04 12:39:51 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    email: { type: String, unique: true },
-    password: String,
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
     created_at: { type: Date, default: Date.now }
 });
 
