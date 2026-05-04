45a312741494108f22b461cc2b436acf63eeb5f6
vk-traders-318 <vktraders.office.1@gmail.com>
2026-05-04 10:19:03 +0530

    Update chat.js

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -4,36 +4,46 @@ const Message = require("../models/Message");
 
 // Send message
 router.post("/send-message", async function (req, res) {
-    const { sender, receiver, text, media } = req.body;
+    try {
+        const { sender, receiver, text, media } = req.body;
 
-    const msg = await Message.create({
-        sender,
-        receiver,
-        text,
-        media
-    });
+        const msg = await Message.create({
+            sender,
+            receiver,
+            text,
+            media
+        });
 
-    res.json({ status: "ok", message: msg });
+        res.json({ status: "ok", message: msg });
+
+    } catch (err) {
+        res.json({ status: "error", msg: "Send failed" });
+    }
 });
 
-// Get messages (Polling)
+// Get messages
 router.get("/get-messages", async function (req, res) {
-    const { sender, receiver, lastId } = req.query;
+    try {
+        const { sender, receiver, lastId } = req.query;
 
-    let query = {
-        $or: [
-            { sender: sender, receiver: receiver },
-            { sender: receiver, receiver: sender }
-        ]
-    };
+        let query = {
+            $or: [
+                { sender, receiver },
+                { sender: receiver, receiver: sender }
+            ]
+        };
 
-    if (lastId) {
-        query._id = { $gt: lastId };
+        if (lastId) {
+            query._id = { $gt: lastId };
+        }
+
+        const messages = await Message.find(query).sort({ created_at: 1 });
+
+        res.json({ status: "ok", messages });
+
+    } catch (err) {
+        res.json({ status: "error", msg: "Fetch failed" });
     }
-
-    const messages = await Message.find(query).sort({ created_at: 1 });
-
-    res.json({ status: "ok", messages: messages });
 });
 
 module.exports = router;
