79d2b47b0b3910e5fdfa7ba24b73e8328a5c3e58
vk-traders-318 <vktraders.office.1@gmail.com>
2026-05-04 10:06:59 +0530

    Update mail.js

--- server/utils/mail.js
+++ server/utils/mail.js
@@ -1,1 +1,22 @@
+const nodemailer = require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+function sendOTP(email, otp) {
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: "Your OTP Code",
+        text: "Your OTP is: " + otp
+    };
+
+    return transporter.sendMail(mailOptions);
+}
+
+module.exports = { sendOTP };
